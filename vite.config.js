a95773dde84257178407e0d6e264dd974dccf330
Tejas-Naik <rntejas2005@gmail.com>
2024-11-23 21:39:15 +0530

    Fixed prod err 2

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
+    minify: 'esbuild',
+    target: 'es2015',
     rollupOptions: {
       output: {
         manualChunks: (id) => {
